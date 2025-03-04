483b277e6bc8b1a5b64a32748de12a62c6c05f31
abdulkabir18 <abdulkabirfagbohun@gmail.com>
2025-03-04 16:02:50 +0100

    First Commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿// 
+﻿// Invoking a method that takes three numbers as input and returns the sum of the three numbers
+Console.WriteLine("Enter your first number");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your second number");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your third number");
+int num3 = int.Parse(Console.ReadLine());
+
+int result = AddThreeNumbers(num1, num2, num3);
+Console.WriteLine($"The sum of {num1}, {num2} and {num3} is {result}");
+
+
+// Method of Adding three numbers
+int AddThreeNumbers(int a, int b, int c)
+{
+    return a + b + c;
+}
